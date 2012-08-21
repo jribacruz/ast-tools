960c22a4ed9e8e106f0c656130321d3001e08d19
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-21 19:48:07 -0300

    delete ast-commons

--- ast-commons/src/ast/commons/core/ASTPredicate.java
+++
@@ -1,7 +0,0 @@
-package ast.commons.core;
-
-import ast.commons.context.ASTContext;
-
-public interface ASTPredicate {
-	public boolean apply(ASTContext context);
-}
--- ast-commons/src/ast/commons/observer/ASTAttributeObserver.java
+++
@@ -1,9 +0,0 @@
-package ast.commons.observer;
-
-import java.util.Set;
-
-import ast.commons.model.Annotation;
-
-public interface ASTAttributeObserver {
-	void update(String name, String type, String genericType, Set<Annotation> annotations);
-}
