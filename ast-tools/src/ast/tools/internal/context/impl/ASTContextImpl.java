426309608b6c85c29a2d4d72b4e403c68e6a2576
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-31 11:27:18 -0300

    add de novos metodos

--- ast-tools/src/ast/tools/internal/context/impl/ASTContextImpl.java
+++ ast-tools/src/ast/tools/internal/context/impl/ASTContextImpl.java
@@ -3,29 +3,40 @@ package ast.tools.internal.context.impl;
 import java.util.Set;
 
 import ast.tools.context.ASTContext;
-import ast.tools.model.Annotation;
-import ast.tools.model.Attribute;
+import ast.tools.model.TAnnotation;
+import ast.tools.model.TAttribute;
+import ast.tools.model.TImport;
+import ast.tools.model.TMethod;
 
 public class ASTContextImpl implements ASTContext {
 
 	private boolean isInterface;
 	private String className;
 	private String superClassName;
-	private Set<Annotation> annotations;
-	private Set<Attribute> attributes;
+	private Set<TAnnotation> annotations;
+	private Set<TAttribute> attributes;
+	private Set<TMethod> methods;
+	private Set<TImport> imports;
+	private Set<String> interfaces;
+	private String packageName;
 
 	public ASTContextImpl() {
 		super();
 	}
 
-	public ASTContextImpl(boolean isInterface, String className, String superClassName, Set<Annotation> annotations,
-			Set<Attribute> attributes) {
+	public ASTContextImpl(boolean isInterface, String className, String superClassName, Set<TAnnotation> annotations,
+			Set<TAttribute> attributes, Set<TMethod> methods, Set<TImport> imports, Set<String> interfaces,
+			String packageName) {
 		super();
 		this.isInterface = isInterface;
 		this.className = className;
 		this.superClassName = superClassName;
 		this.annotations = annotations;
 		this.attributes = attributes;
+		this.methods = methods;
+		this.imports = imports;
+		this.interfaces = interfaces;
+		this.packageName = packageName;
 	}
 
 	@Override
@@ -44,13 +55,33 @@ public class ASTContextImpl implements ASTContext {
 	}
 
 	@Override
-	public Set<Annotation> getAnnotations() {
+	public Set<TAnnotation> getAnnotations() {
 		return this.annotations;
 	}
 
 	@Override
-	public Set<Attribute> getAttributes() {
+	public Set<TAttribute> getAttributes() {
 		return this.attributes;
+	}
+
+	@Override
+	public Set<TMethod> getMethods() {
+		return this.methods;
+	}
+
+	@Override
+	public Set<TImport> getImports() {
+		return this.imports;
+	}
+
+	@Override
+	public Set<String> getInterfaces() {
+		return this.interfaces;
+	}
+
+	@Override
+	public String getPackageName() {
+		return this.packageName;
 	}
 
 }
