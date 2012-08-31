69a551a6c07049ef4e7ab300c05bdac21008f7b0
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-31 16:09:50 -0300

    @SuppressWarnings

--- ast-tools/src/ast/tools/model/TModifier.java
+++ ast-tools/src/ast/tools/model/TModifier.java
@@ -4,6 +4,7 @@ import org.eclipse.jdt.core.dom.Modifier;
 
 public enum TModifier {
 	PUBLIC(Modifier.PUBLIC), PROTECTED(Modifier.PROTECTED), PRIVATE(Modifier.PRIVATE), ABSTRACT(Modifier.ABSTRACT), FINAL(Modifier.FINAL), STATIC(Modifier.STATIC), VOLATILE(Modifier.VOLATILE), TRANSIENT(Modifier.TRANSIENT), SYNCHRONIZED(Modifier.SYNCHRONIZED), NATIVE(Modifier.NATIVE), STRICTFP(Modifier.STRICTFP);
+	@SuppressWarnings("unused")
 	private int modifierType;
 
 	private TModifier(int modifierType) {
