a71fe61bdbcf473e74b0bc8f939509bd57e45f75
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-31 12:12:58 -0300

    novo hashCode e equal e toString

--- ast-tools/src/ast/tools/internal/model/impl/TParameterImpl.java
+++ ast-tools/src/ast/tools/internal/model/impl/TParameterImpl.java
@@ -30,4 +30,72 @@ public class TParameterImpl implements TParameter {
 		return this.genericType;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((genericType == null) ? 0 : genericType.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		TParameterImpl other = (TParameterImpl) obj;
+		if (genericType == null) {
+			if (other.genericType != null) {
+				return false;
+			}
+		} else if (!genericType.equals(other.genericType)) {
+			return false;
+		}
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		if (type == null) {
+			if (other.type != null) {
+				return false;
+			}
+		} else if (!type.equals(other.type)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TParameterImpl [");
+		if (name != null) {
+			builder.append("name=");
+			builder.append(name);
+			builder.append(", ");
+		}
+		if (type != null) {
+			builder.append("type=");
+			builder.append(type);
+			builder.append(", ");
+		}
+		if (genericType != null) {
+			builder.append("genericType=");
+			builder.append(genericType);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
 }
