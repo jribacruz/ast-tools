52b8cf6b2d0cf9b53b2cc22697f2dfaa82441094
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-08-30 22:22:17 -0300

    altecao no nome do pacote

---
+++ ast-tools/src/ast/tools/internal/model/impl/AnnotationImpl.java
@@ -0,0 +1,129 @@
+package ast.tools.internal.model.impl;
+
+import java.util.ArrayList;
+
+import ast.tools.model.Annotation;
+
+import com.google.common.collect.Multimap;
+
+public class AnnotationImpl implements Annotation {
+
+	private String name;
+
+	private Multimap<String, Object> value;
+
+	public AnnotationImpl() {
+		super();
+	}
+
+	public AnnotationImpl(String name, Multimap<String, Object> value) {
+		super();
+		this.name = name;
+		this.value = value;
+	}
+
+	public AnnotationImpl(String name) {
+		super();
+		this.name = name;
+	}
+
+	@Override
+	public String getName() {
+		return this.name;
+	}
+
+	@Override
+	public String getString(String key) {
+		return (String) (this.value.containsKey(key) && this.value.size() > 0 ? new ArrayList<Object>(this.value.get(key))
+				.get(0) : new String());
+	}
+
+	@Override
+	public Integer getInteger(String key) {
+		return (Integer) (this.value.containsKey(key) && this.value.size() > 0 ? new ArrayList<Object>(this.value.get(key))
+				.get(0) : new Integer(0));
+	}
+
+	@Override
+	public Long getLong(String key) {
+		return (Long) (this.value.containsKey(key) && this.value.size() > 0 ? new ArrayList<Object>(this.value.get(key))
+				.get(0) : new Long(0L));
+	}
+
+	@Override
+	public Class<?> getKlass(String key) {
+		return null;
+	}
+
+	@Override
+	public String[] getStringArray(String key) {
+		return (this.value.containsKey(key) && this.value.size() > 0 ? new ArrayList<Object>(this.value.get(key))
+				.toArray(new String[] {}) : new String[] {});
+	}
+
+	@Override
+	public Integer[] getIntegerArray(String key) {
+		return (this.value.containsKey(key) && this.value.size() > 0 ? new ArrayList<Object>(this.value.get(key))
+				.toArray(new Integer[] {}) : new Integer[] {});
+	}
+
+	@Override
+	public Long[] getLongArray(String key) {
+		return (this.value.containsKey(key) && this.value.size() > 0 ? new ArrayList<Object>(this.value.get(key))
+				.toArray(new Long[] {}) : new Long[] {});
+	}
+
+	@Override
+	public Annotation getAnnotation(String key) {
+		return (Annotation) (this.value.containsKey(key) && this.value.size() > 0 ? new ArrayList<Object>(
+				this.value.get(key)).get(0) : new AnnotationImpl());
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		AnnotationImpl other = (AnnotationImpl) obj;
+		if (name == null) {
+			if (other.name != null) {
+				return false;
+			}
+		} else if (!name.equals(other.name)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("Annotation [");
+		if (name != null) {
+			builder.append("name=");
+			builder.append(name);
+			builder.append(", ");
+		}
+		if (value != null) {
+			builder.append("value=");
+			builder.append(value);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
+}
