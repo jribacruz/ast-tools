f81868248b2292840a4fd4250d4970f06b902cfd
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-09-03 02:04:54 -0300

    inclusao do metodo getModifierType

--- ast-tools/src/ast/tools/model/TModifier.java
+++ ast-tools/src/ast/tools/model/TModifier.java
@@ -3,13 +3,17 @@ package ast.tools.model;
 import org.eclipse.jdt.core.dom.Modifier;
 
 public enum TModifier {
-	PUBLIC(Modifier.PUBLIC), PROTECTED(Modifier.PROTECTED), PRIVATE(Modifier.PRIVATE), ABSTRACT(Modifier.ABSTRACT), FINAL(Modifier.FINAL), STATIC(Modifier.STATIC), VOLATILE(Modifier.VOLATILE), TRANSIENT(Modifier.TRANSIENT), SYNCHRONIZED(Modifier.SYNCHRONIZED), NATIVE(Modifier.NATIVE), STRICTFP(Modifier.STRICTFP);
-	@SuppressWarnings("unused")
+	PUBLIC(Modifier.PUBLIC), PROTECTED(Modifier.PROTECTED), PRIVATE(Modifier.PRIVATE), ABSTRACT(Modifier.ABSTRACT), FINAL(
+			Modifier.FINAL), STATIC(Modifier.STATIC), VOLATILE(Modifier.VOLATILE), TRANSIENT(Modifier.TRANSIENT), SYNCHRONIZED(
+					Modifier.SYNCHRONIZED), NATIVE(Modifier.NATIVE), STRICTFP(Modifier.STRICTFP);
 	private int modifierType;
 
 	private TModifier(int modifierType) {
 		this.modifierType = modifierType;
 	}
 
+	public int getModifierType() {
+		return modifierType;
+	}
 
 }
