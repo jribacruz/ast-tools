a780b309db424cab4e0d02262f195ac747930dfc
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-09-03 14:31:18 -0300

    refatoracao da classe ASTUtils

--- ast-tools/src/ast/tools/util/ASTUtils.java
+++ ast-tools/src/ast/tools/util/ASTUtils.java
@@ -2,7 +2,6 @@ package ast.tools.util;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 import org.eclipse.jdt.core.ICompilationUnit;
 
@@ -10,8 +9,7 @@ import ast.tools.context.ASTContext;
 import ast.tools.context.ASTWriter;
 import ast.tools.core.ASTPredicate;
 import ast.tools.core.ASTProcessor;
-import ast.tools.internal.model.impl.TAnnotationImpl;
-import ast.tools.model.TAnnotation;
+import ast.tools.model.TClass;
 
 public class ASTUtils {
 	public static ICompilationUnit find(ICompilationUnit[] units, ASTPredicate predicate) {
@@ -21,6 +19,11 @@ public class ASTUtils {
 			return context != null && predicate.apply(context) ? unit : null;
 		}
 		return null;
+	}
+
+	public static TClass findTClass(ICompilationUnit[] units, ASTPredicate predicate) {
+		ICompilationUnit unit = find(units, predicate);
+		return unit != null ? getTClass(unit) : null;
 	}
 
 	public static ICompilationUnit[] filter(ICompilationUnit[] units, ASTPredicate predicate) {
@@ -44,8 +47,12 @@ public class ASTUtils {
 		}
 	}
 
-	public static boolean containsAnnotation(Set<TAnnotation> annotations, String name) {
-		return annotations.contains(new TAnnotationImpl(name));
+	public static TClass getTClass(ICompilationUnit iunit) {
+		ASTProcessor processor = new ASTProcessor(iunit);
+		processor.visit();
+		return processor.getTClass();
 	}
+
+
 
 }
