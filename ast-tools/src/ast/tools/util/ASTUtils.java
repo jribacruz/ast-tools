4bb2c78715b438a5f1b529f03621bd6ee23438ee
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-09-06 14:15:46 -0300

    suporte a modifiers

--- ast-tools/src/ast/tools/util/ASTUtils.java
+++ ast-tools/src/ast/tools/util/ASTUtils.java
@@ -1,15 +1,20 @@
 package ast.tools.util;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.dom.PrimitiveType;
+import org.eclipse.jface.dialogs.MessageDialog;
 
 import ast.tools.context.ASTContext;
 import ast.tools.context.ASTWriter;
 import ast.tools.core.ASTPredicate;
 import ast.tools.core.ASTProcessor;
 import ast.tools.model.TClass;
+import ast.tools.model.TModifier;
 
 public class ASTUtils {
 	public static ICompilationUnit find(ICompilationUnit[] units, ASTPredicate predicate) {
@@ -53,6 +58,43 @@ public class ASTUtils {
 		return processor.getTClass();
 	}
 
+	public static boolean isPrimitiveType(String type) {
+		return getPrimitiveType(type) != null;
+	}
 
+	public static PrimitiveType.Code getPrimitiveType(String type) {
+		if (type.equals("int")) {
+			return PrimitiveType.INT;
+		} else if (type.equals("float")) {
+			return PrimitiveType.FLOAT;
+		} else if (type.equals("double")) {
+			return PrimitiveType.DOUBLE;
+		} else if (type.equals("char")) {
+			return PrimitiveType.CHAR;
+		} else if (type.equals("boolean")) {
+			return PrimitiveType.BOOLEAN;
+		} else if (type.equals("byte")) {
+			return PrimitiveType.BYTE;
+		} else if (type.equals("long")) {
+			return PrimitiveType.LONG;
+		} else if (type.equals("short")) {
+			return PrimitiveType.SHORT;
+		} else if (type.equals("void")) {
+			return PrimitiveType.VOID;
+		}
+		return null;
+	}
+
+	public static Set<TModifier> getModifiers(String... modifiers) {
+		Set<TModifier> modifiersSet = new HashSet<TModifier>();
+		for (String modifier : modifiers) {
+			try {
+				modifiersSet.add(TModifier.valueOf(modifier.toUpperCase()));
+			} catch (IllegalArgumentException e) {
+				MessageDialog.openError(null, "Erro", "O tipo " + modifier + " não exite");
+			}
+		}
+		return modifiersSet;
+	}
 
 }
