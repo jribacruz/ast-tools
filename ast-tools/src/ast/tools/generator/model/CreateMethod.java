6324494c1a728cd9ed97c7f0f21e571658dea662
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-09-10 20:38:50 -0300

    impl do apply

--- ast-tools/src/ast/tools/generator/model/CreateMethod.java
+++ ast-tools/src/ast/tools/generator/model/CreateMethod.java
@@ -2,21 +2,28 @@ package ast.tools.generator.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlList;
 import javax.xml.bind.annotation.XmlType;
 
-import org.apache.commons.lang.StringUtils;
+import org.eclipse.jdt.core.dom.AST;
 
+import ast.tools.context.ASTWriter;
 import ast.tools.generator.context.GeneratorContext;
+import ast.tools.generator.core.IGeneratorElement;
+import ast.tools.helper.CompilationUnitHelper;
+import ast.tools.helper.MethodDeclarationHelper;
+import ast.tools.model.TModifier;
 
 @XmlType
 @XmlAccessorType(XmlAccessType.FIELD)
-public class CreateMethod {
+public class CreateMethod implements IGeneratorElement {
 
 	@XmlAttribute(required = true)
 	private String name;
@@ -25,10 +32,18 @@ public class CreateMethod {
 	private String returnGenericType;
 
 	@XmlAttribute
-	private String returnTypes;
+	@XmlList
+	private List<String> returnTypes;
 
 	@XmlAttribute
-	private String modifiers;
+	private boolean nullReturn;
+
+	@XmlAttribute
+	private boolean constructor;
+
+	@XmlAttribute
+	@XmlList
+	private Set<TModifier> modifiers;
 
 	@XmlElementWrapper(name = "parameters")
 	@XmlElement(name = "parameter")
@@ -42,6 +57,7 @@ public class CreateMethod {
 		super();
 		this.parameters = new ArrayList<Parameter>();
 		this.annotations = new ArrayList<Annotation>();
+		this.returnTypes= new ArrayList<String>();
 	}
 
 	public String getName() {
@@ -77,19 +93,35 @@ public class CreateMethod {
 	}
 
 	public String[] getReturnTypes() {
-		return StringUtils.split(returnTypes, ",");
+		return returnTypes.toArray(new String[] {});
 	}
 
-	public void setReturnTypes(String returnTypes) {
+	public void setReturnTypes(List<String> returnTypes) {
 		this.returnTypes = returnTypes;
 	}
 
-	public String[] getModifiers() {
-		return StringUtils.split(modifiers, ",");
+	public Set<TModifier> getModifiers() {
+		return modifiers;
 	}
 
-	public void setModifiers(String modifiers) {
+	public void setModifiers(Set<TModifier> modifiers) {
 		this.modifiers = modifiers;
+	}
+
+	public boolean isNullReturn() {
+		return nullReturn;
+	}
+
+	public void setNullReturn(boolean nullReturn) {
+		this.nullReturn = nullReturn;
+	}
+
+	public boolean isConstructor() {
+		return constructor;
+	}
+
+	public void setConstructor(boolean constructor) {
+		this.constructor = constructor;
 	}
 
 	@Override
@@ -127,6 +159,31 @@ public class CreateMethod {
 		}
 		builder.append("]");
 		return builder.toString();
+	}
+
+	@Override
+	public ASTWriter apply() {
+		return new ASTWriter() {
+
+			@Override
+			public void write(CompilationUnitHelper unitHelper, AST ast) {
+				MethodDeclarationHelper helper;
+				if (constructor) {
+					helper = new MethodDeclarationHelper(ast, getName(), modifiers, constructor);
+					unitHelper.addMethod(helper.getMethodDeclaration());
+				} else {
+					helper = new MethodDeclarationHelper(ast, getName(), modifiers);
+					helper.setReturnType(getReturnGenericType(), getReturnTypes());
+					for (Parameter p : getParameters()) {
+						helper.addParameter(p.getName(), p.getGenericType(), p.getTypes());
+					}
+					if(isNullReturn()) {
+						helper.setNullReturn();
+					}
+					unitHelper.addMethod(helper.getMethodDeclaration());
+				}
+			}
+		};
 	}
 
 }
