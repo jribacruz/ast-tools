eff6b362ff24b04d2ae504e005853a8ae8a99ae6
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-09-10 19:02:54 -0300

    impl do metodo apply

--- ast-tools/src/ast/tools/generator/model/CreateField.java
+++ ast-tools/src/ast/tools/generator/model/CreateField.java
@@ -1,31 +1,43 @@
 package ast.tools.generator.model;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlList;
 import javax.xml.bind.annotation.XmlType;
 
 import org.apache.commons.lang.StringUtils;
+import org.eclipse.jdt.core.dom.AST;
 
+import ast.tools.context.ASTWriter;
 import ast.tools.generator.context.GeneratorContext;
+import ast.tools.generator.core.IGeneratorElement;
+import ast.tools.helper.CompilationUnitHelper;
+import ast.tools.helper.FieldDeclarationHelper;
+import ast.tools.model.TModifier;
+import ast.tools.util.TUtils;
 
 @XmlType
 @XmlAccessorType(XmlAccessType.FIELD)
-public class CreateField {
+public class CreateField implements IGeneratorElement {
 
 	@XmlAttribute(required = true)
 	private String name;
 
 	@XmlAttribute
-	private String modifiers;
+	@XmlList
+	private Set<TModifier> modifiers;
 
 	@XmlAttribute
-	private String types;
+	@XmlList
+	private List<String> types;
 
 	@XmlAttribute
 	private String genericType;
@@ -37,22 +49,17 @@ public class CreateField {
 	public CreateField() {
 		super();
 		this.annotations = new ArrayList<Annotation>();
+		this.modifiers = new HashSet<TModifier>();
+		this.types = new ArrayList<String>();
 	}
 
 	public String getName() {
+		// substitui os tokens do nome
 		return GeneratorContext.replaceTokens(name);
 	}
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public String[] getTypes() {
-		return StringUtils.split(types, ",");
-	}
-
-	public void setTypes(String types) {
-		this.types = types;
 	}
 
 	public String getGenericType() {
@@ -63,20 +70,71 @@ public class CreateField {
 		this.genericType = genericType;
 	}
 
-	public String[] getModifiers() {
-		return StringUtils.split(this.modifiers, ",");
-	}
-
-	public void setModifiers(String modifiers) {
-		this.modifiers = modifiers;
-	}
-
 	public List<Annotation> getAnnotations() {
 		return annotations;
 	}
 
 	public void setAnnotations(List<Annotation> annotations) {
 		this.annotations = annotations;
+	}
+
+	public String[] getTypes() {
+		return types.toArray(new String[] {});
+	}
+
+	public void setTypes(List<String> types) {
+		this.types = types;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("CreateField [");
+		if (name != null) {
+			builder.append("name=");
+			builder.append(name);
+			builder.append(", ");
+		}
+		if (modifiers != null) {
+			builder.append("modifiers=");
+			builder.append(modifiers);
+			builder.append(", ");
+		}
+		if (types != null) {
+			builder.append("types=");
+			builder.append(types);
+			builder.append(", ");
+		}
+		if (genericType != null) {
+			builder.append("genericType=");
+			builder.append(genericType);
+			builder.append(", ");
+		}
+		if (annotations != null) {
+			builder.append("annotations=");
+			builder.append(annotations);
+		}
+		builder.append("]");
+		return builder.toString();
+	}
+
+	@Override
+	public ASTWriter apply() {
+		return new ASTWriter() {
+
+			@Override
+			public void write(CompilationUnitHelper unitHelper, AST ast) {
+				FieldDeclarationHelper helper = new FieldDeclarationHelper(ast, getName(), modifiers);
+				if (!StringUtils.isEmpty(getGenericType())) {
+					helper.setType(genericType, getTypes());
+				} else if (getTypes().length == 1 && TUtils.isPrimitiveType(getTypes()[0])) {
+					helper.setType(TUtils.stringToPrimitiveCode(getTypes()[0]));
+				} else if (getTypes().length == 1) {
+					helper.setType(getTypes()[0]);
+				}
+				unitHelper.addFieldDeclaration(helper.getFieldDeclaration());
+			}
+		};
 	}
 
 }
