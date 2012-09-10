68fef364ead7c2a5c7e94c9d89cb3f44d70a5b9e
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-09-10 11:20:23 -0300

    add de annotations JAXB nos enums

--- ast-tools/src/ast/tools/model/TModifier.java
+++ ast-tools/src/ast/tools/model/TModifier.java
@@ -1,11 +1,47 @@
 package ast.tools.model;
 
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+import javax.xml.bind.annotation.XmlType;
+
 import org.eclipse.jdt.core.dom.Modifier;
 
+@XmlType
+@XmlEnum(String.class)
 public enum TModifier {
-	PUBLIC(Modifier.PUBLIC), PROTECTED(Modifier.PROTECTED), PRIVATE(Modifier.PRIVATE), ABSTRACT(Modifier.ABSTRACT), FINAL(
-			Modifier.FINAL), STATIC(Modifier.STATIC), VOLATILE(Modifier.VOLATILE), TRANSIENT(Modifier.TRANSIENT), SYNCHRONIZED(
-					Modifier.SYNCHRONIZED), NATIVE(Modifier.NATIVE), STRICTFP(Modifier.STRICTFP);
+	@XmlEnumValue("public")
+	PUBLIC(Modifier.PUBLIC),
+
+	@XmlEnumValue("protected")
+	PROTECTED(Modifier.PROTECTED),
+
+	@XmlEnumValue("private")
+	PRIVATE(Modifier.PRIVATE),
+
+	@XmlEnumValue("abstract")
+	ABSTRACT(Modifier.ABSTRACT),
+
+	@XmlEnumValue("final")
+	FINAL(Modifier.FINAL),
+
+	@XmlEnumValue("static")
+	STATIC(Modifier.STATIC),
+
+	@XmlEnumValue("volatile")
+	VOLATILE(Modifier.VOLATILE),
+
+	@XmlEnumValue("trasient")
+	TRANSIENT(Modifier.TRANSIENT),
+
+	@XmlEnumValue("synchronized")
+	SYNCHRONIZED(Modifier.SYNCHRONIZED),
+
+	@XmlEnumValue("native")
+	NATIVE(Modifier.NATIVE),
+
+	@XmlEnumValue("stricfp")
+	STRICTFP(Modifier.STRICTFP);
+
 	private int modifierType;
 
 	private TModifier(int modifierType) {
