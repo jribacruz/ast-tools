dc789544f50b2bdc30025469ce2bbf566f14921e
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-09-13 21:03:49 -0300

    remoção de métodos

--- ast-tools/src/ast/tools/util/ASTUtils.java
+++ ast-tools/src/ast/tools/util/ASTUtils.java
@@ -1,18 +1,13 @@
 package ast.tools.util;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.eclipse.jdt.core.ICompilationUnit;
-import org.eclipse.jdt.core.dom.PrimitiveType;
-import org.eclipse.jface.dialogs.MessageDialog;
 
 import ast.tools.context.ASTContext;
 import ast.tools.core.ASTProcessor;
 import ast.tools.model.TClass;
-import ast.tools.model.TModifier;
 
 import com.google.common.base.Predicate;
 
@@ -47,45 +42,6 @@ public class ASTUtils {
 		ASTProcessor processor = new ASTProcessor(iunit);
 		processor.visit();
 		return processor.getTClass();
-	}
-
-	public static boolean isPrimitiveType(String type) {
-		return getPrimitiveType(type) != null;
-	}
-
-	public static PrimitiveType.Code getPrimitiveType(String type) {
-		if (type.equals("int")) {
-			return PrimitiveType.INT;
-		} else if (type.equals("float")) {
-			return PrimitiveType.FLOAT;
-		} else if (type.equals("double")) {
-			return PrimitiveType.DOUBLE;
-		} else if (type.equals("char")) {
-			return PrimitiveType.CHAR;
-		} else if (type.equals("boolean")) {
-			return PrimitiveType.BOOLEAN;
-		} else if (type.equals("byte")) {
-			return PrimitiveType.BYTE;
-		} else if (type.equals("long")) {
-			return PrimitiveType.LONG;
-		} else if (type.equals("short")) {
-			return PrimitiveType.SHORT;
-		} else if (type.equals("void")) {
-			return PrimitiveType.VOID;
-		}
-		return null;
-	}
-
-	public static Set<TModifier> getModifiers(String... modifiers) {
-		Set<TModifier> modifiersSet = new HashSet<TModifier>();
-		for (String modifier : modifiers) {
-			try {
-				modifiersSet.add(TModifier.valueOf(modifier.toUpperCase()));
-			} catch (IllegalArgumentException e) {
-				MessageDialog.openError(null, "Erro", "O tipo " + modifier + " não exite");
-			}
-		}
-		return modifiersSet;
 	}
 
 }
