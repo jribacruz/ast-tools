acc67df0a83f7bb7e4745ccb3178843b49d18f0b
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-09-13 22:14:46 -0300

    remocao das annotations JAXB

--- ast-tools/src/ast/tools/model/TModifier.java
+++ ast-tools/src/ast/tools/model/TModifier.java
@@ -1,45 +1,27 @@
 package ast.tools.model;
 
-import javax.xml.bind.annotation.XmlEnum;
-import javax.xml.bind.annotation.XmlEnumValue;
-import javax.xml.bind.annotation.XmlType;
-
 import org.eclipse.jdt.core.dom.Modifier;
-
-@XmlType
-@XmlEnum(String.class)
 public enum TModifier {
-	@XmlEnumValue("public")
 	PUBLIC(Modifier.PUBLIC),
 
-	@XmlEnumValue("protected")
 	PROTECTED(Modifier.PROTECTED),
 
-	@XmlEnumValue("private")
 	PRIVATE(Modifier.PRIVATE),
 
-	@XmlEnumValue("abstract")
 	ABSTRACT(Modifier.ABSTRACT),
 
-	@XmlEnumValue("final")
 	FINAL(Modifier.FINAL),
 
-	@XmlEnumValue("static")
 	STATIC(Modifier.STATIC),
 
-	@XmlEnumValue("volatile")
 	VOLATILE(Modifier.VOLATILE),
 
-	@XmlEnumValue("trasient")
 	TRANSIENT(Modifier.TRANSIENT),
 
-	@XmlEnumValue("synchronized")
 	SYNCHRONIZED(Modifier.SYNCHRONIZED),
 
-	@XmlEnumValue("native")
 	NATIVE(Modifier.NATIVE),
 
-	@XmlEnumValue("stricfp")
 	STRICTFP(Modifier.STRICTFP);
 
 	private int modifierType;
