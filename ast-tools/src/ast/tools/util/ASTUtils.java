8945cba93796cf88f1012743d2c9703b0727029b
Jos Ribamar Monteiro da Cruz <jribacruz@gmail.com>
2012-10-11 18:22:28 -0300

    alteracoes no metodo find e filter

--- ast-tools/src/ast/tools/util/ASTUtils.java
+++ ast-tools/src/ast/tools/util/ASTUtils.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.JavaModelException;
 
 import ast.tools.context.ASTContext;
 import ast.tools.core.ASTProcessor;
@@ -21,12 +24,35 @@ public class ASTUtils {
 		return null;
 	}
 
-	public static TClass findTClass(ICompilationUnit[] units, Predicate<ASTContext> predicate) {
-		ICompilationUnit unit = find(units, predicate);
-		return unit != null ? getTClass(unit) : null;
+	public static ICompilationUnit find(IJavaProject javaProject, Predicate<ASTContext> predicate) {
+		try {
+			for (IPackageFragment fragment : javaProject.getPackageFragments()) {
+				for (ICompilationUnit unit : fragment.getCompilationUnits()) {
+					ASTProcessor processor = new ASTProcessor(unit);
+					ASTContext context = processor.visit();
+					return context != null && predicate.apply(context) ? unit : null;
+				}
+			}
+		} catch (JavaModelException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	public static ICompilationUnit[] filter(ICompilationUnit[] units, Predicate<ASTContext> predicate) {
+	public static ICompilationUnit find(IPackageFragment fragment, Predicate<ASTContext> predicate) {
+		try {
+			for (ICompilationUnit unit : fragment.getCompilationUnits()) {
+				ASTProcessor processor = new ASTProcessor(unit);
+				ASTContext context = processor.visit();
+				return context != null && predicate.apply(context) ? unit : null;
+			}
+		} catch (JavaModelException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static List<ICompilationUnit> filter(ICompilationUnit[] units, Predicate<ASTContext> predicate) {
 		List<ICompilationUnit> unitList = new ArrayList<ICompilationUnit>();
 		for (ICompilationUnit unit : units) {
 			ASTProcessor processor = new ASTProcessor(unit);
@@ -35,7 +61,41 @@ public class ASTUtils {
 				unitList.add(unit);
 			}
 		}
-		return unitList.toArray(new ICompilationUnit[] {});
+		return unitList;
+	}
+
+	public static List<ICompilationUnit> filter(IJavaProject javaProject, Predicate<ASTContext> predicate) {
+		List<ICompilationUnit> unitList = new ArrayList<ICompilationUnit>();
+		try {
+			for (IPackageFragment fragment : javaProject.getPackageFragments()) {
+				for (ICompilationUnit unit : fragment.getCompilationUnits()) {
+					ASTProcessor processor = new ASTProcessor(unit);
+					ASTContext context = processor.visit();
+					if (context != null && predicate.apply(context)) {
+						unitList.add(unit);
+					}
+				}
+			}
+		} catch (JavaModelException e) {
+			e.printStackTrace();
+		}
+		return unitList;
+	}
+
+	public static List<ICompilationUnit> filter(IPackageFragment fragment, Predicate<ASTContext> predicate) {
+		List<ICompilationUnit> unitList = new ArrayList<ICompilationUnit>();
+		try {
+			for (ICompilationUnit unit : fragment.getCompilationUnits()) {
+				ASTProcessor processor = new ASTProcessor(unit);
+				ASTContext context = processor.visit();
+				if (context != null && predicate.apply(context)) {
+					unitList.add(unit);
+				}
+			}
+		} catch (JavaModelException e) {
+			e.printStackTrace();
+		}
+		return unitList;
 	}
 
 	public static TClass getTClass(ICompilationUnit iunit) {
